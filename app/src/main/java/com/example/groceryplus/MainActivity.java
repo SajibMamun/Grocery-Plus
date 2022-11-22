2610520115f73a7e6c8efa353b8ea45cc751ce18
Sajib Bin Mamun <sajibmamun138@gmail.com>
2022-11-23 02:37:38 +0600

    NavDrawer Complete

--- app/src/main/java/com/example/groceryplus/MainActivity.java
+++ app/src/main/java/com/example/groceryplus/MainActivity.java
@@ -3,10 +3,14 @@ package com.example.groceryplus;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.example.groceryplus.Models.UserDataModel;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -15,11 +19,19 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.groceryplus.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    FirebaseDatabase firebaseDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +54,33 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+
+   View headerview=navigationView.getHeaderView(0);
+        TextView Headername=headerview.findViewById(R.id.nav_header_name);
+        TextView Headeremail=headerview.findViewById(R.id.nav_header_email);
+        firebaseDatabase=FirebaseDatabase.getInstance();
+        CircleImageView HeaderImg=headerview.findViewById(R.id.nav_header_img);
+
+
+        firebaseDatabase.getReference().child("Users").child(FirebaseAuth.getInstance().getUid())
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        UserDataModel userDataModel=snapshot.getValue(UserDataModel.class);
+                        Headername.setText(userDataModel.getName());
+                        Headeremail.setText(userDataModel.getEmail());
+                      Glide.with(MainActivity.this).load(userDataModel.getProfileImg()).into(HeaderImg);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+
+
     }
 
     @Override
